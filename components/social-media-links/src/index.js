98b17caf4f9ec7625d2a1bf7159e7e1a5081b412
Hannes Elvemyr <hannes@accodeing.com>
2019-12-17 13:22:12 +0100

    Adds oma-social-media-links

---
+++ components/social-media-links/src/index.js
@@ -0,0 +1,27 @@
+import { component, html } from 'haunted'
+
+const Links = ({ socialMedias }) => {
+  return html`
+    <ul class="oma-social-media-links">
+      <slot></slot>
+    </ul>
+  `
+}
+
+customElements.define('oma-social-media-links', component(Links))
+
+const Instagram = ({ accountId }) => {
+  const baseUrl = 'https://www.instagram.com/'
+
+  return html`
+    <a
+      class="oma-social-media-link"
+      href="${baseUrl}${accountId}"
+      target="_blank"
+    >
+      <slot></slot>
+    </a>
+  `
+}
+
+customElements.define('oma-instagram-link', component(Instagram))
