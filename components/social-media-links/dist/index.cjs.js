aafb375b669624d41c33b413efe6b71f8042177f
Hannes Elvemyr <hannes@accodeing.com>
2019-12-18 13:12:08 +0100

    Fixes broken dep in social-media-links
    Missed to add oma-state as dependency in package.json

--- components/social-media-links/dist/index.cjs.js
+++ components/social-media-links/dist/index.cjs.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var state = require('@oma-wc/state');
-
 /**
  * @license
  * Copyright (c) 2017 The Polymer Project Authors. All rights reserved.
@@ -1550,6 +1548,83 @@ function haunted({ render }) {
 
 const { component, createContext } = haunted({ render });
 
+class StyleSheet {
+  constructor(id) {
+    if (document.getElementById(id)) {
+      this.sheet = document.getElementById(id).sheet;
+    } else {
+      this.sheet = this.create_sheet(id);
+    }
+    this.written_rules = {};
+    this.pending_rules = {};
+  }
+
+  create_sheet(id) {
+    var style_element = document.createElement('style');
+    if (id) {
+      style_element.id = id;
+    }
+    // WebKit hack :(
+    style_element.appendChild(document.createTextNode(''));
+
+    document.head.appendChild(style_element);
+
+    return style_element.sheet
+  }
+
+  add_rule( input ){
+    const matches = input.match(/^([^{]*)\{([^}]*)\}/);
+    const selector = matches[1].trim();
+    const rules = matches[2].trim();
+
+    if( !this.pending_rules[ selector ]){
+      this.pending_rules[ selector ] = `${rules}`;
+    } else {
+      this.pending_rules[ selector ] += `\n${rules}`;
+    }
+  }
+
+  write() {
+    for( let [ selector, rules ] of Object.entries( this.pending_rules )){
+      this.write_rule(selector, rules);
+
+      if( !this.written_rules[ selector ]){
+        this.written_rules[ selector ] = `${rules}`;
+      } else {
+        this.written_rules[ selector ] += `\n${rules}`;
+      }
+
+      delete this.pending_rules[selector];
+    }
+  }
+
+  write_rule(selector, rules) {
+    if ('insertRule' in this.sheet) {
+      this.sheet.insertRule(selector + '{' + rules + '}');
+    } else if ('addRule' in this.sheet) {
+      this.sheet.addRule(selector, rules);
+    }
+  }
+}
+
+Object.defineProperty(StyleSheet, 'pending_rules', {
+  value: {},
+  writable: true,
+});
+
+class Singleton {
+  constructor() {
+    this._stylesheet = new StyleSheet( 'oma-styles' );
+    Object.freeze( this._stylesheet );
+  }
+
+  get stylesheet() {
+    return this._stylesheet;
+  }
+}
+
+const State$1 = Singleton();
+
 const FACEBOOK = 'facebook';
 const INSTAGRAM = 'instagram';
 const LINKEDIN_PRIVATE = 'linkedin-private';
@@ -1565,7 +1640,7 @@ const urls = {
   [TWITTER]: 'https://twitter.com/',
 };
 
-state.State.stylesheet.add_rule(
+State$1.stylesheet.add_rule(
   `oma-social-media-links {
      margin: 0;
      padding: 0;
@@ -1597,7 +1672,7 @@ const Link = ({ accountid, type }) => {
   }
 
   useEffect(() => {
-    state.State.stylesheet.write();
+    State$1.stylesheet.write();
   });
 
   const url = urls[type] + accountid;
