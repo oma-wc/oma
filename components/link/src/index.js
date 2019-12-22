1b558db01c4adb96fd96b523178e4885c0e70a27
Hannes Elvemyr <hannes@accodeing.com>
2019-12-22 14:00:32 +0100

    Adds Link component

---
+++ components/link/src/index.js
@@ -0,0 +1,58 @@
+import { component, html } from 'haunted'
+
+import { State } from '@oma-wc/state'
+
+import { parsePhoneNumberFromString } from 'libphonenumber-js/min'
+
+const ELEMENT = 'oma-link'
+
+State.stylesheet.add_rule(
+  `oma-link {
+    color: var(--oma-link__color);
+    margin: var(--oma-link__margin);
+    padding: var(--oma-link__padding);
+    text_decoration: var(--oma-link__text-decoration, underline);
+  }`
+)
+
+const EmailLink = ({ email }) =>
+  html`
+    ${LinkTag({ content: email, to: `mailto:${email}` })}
+  `
+
+const PhoneLink = ({ number }) =>
+  html`
+    ${
+      LinkTag({
+        content: number.formatNational(),
+        to: `tel:${number.getURI()}`,
+      })
+    }
+  `
+
+const LinkTag = ({ content, to }) => {
+  return html`
+    <a href="${to}"> ${content} <slot></slot> </a>
+  `
+}
+
+const Link = ({ to }) => {
+  if (to.includes('@')) {
+    return html`
+      ${EmailLink({ email: to })}
+    `
+  }
+
+  const phoneNumber = parsePhoneNumberFromString(to)
+  if (phoneNumber.isValid()) {
+    return html`
+      ${PhoneLink({ number: phoneNumber })}
+    `
+  }
+
+  console.error(`ERROR, ${ELEMENT}: Unknown link type. Link to: ${to}`)
+}
+
+Link.observedAttributes = ['to']
+
+customElements.define(ELEMENT, component(Link))
