cd1e1bd5940ec755652e450c9ba396b66b3ac46c
Hannes Elvemyr <hannes@accodeing.com>
2020-03-27 16:41:25 +0100

    Prettifies rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import nodeResolve from 'rollup-plugin-node-resolve'
 //import commonjs from 'rollup-plugin-commonjs'
 //import babel from 'rollup-plugin-babel'
 
-export default opts => {
+export default (opts) => {
   const options = Object.assign(
     {
       css: true,
@@ -19,12 +19,12 @@ export default opts => {
 
     plugins: [
       nodeResolve({}),
-//      commonjs({}),
-//      babel({
-//        runtimeHelpers: true,
-//        exclude: '../../node_modules/**',
-//        configFile: '../../babel.config.js',
-//      }),
+      //      commonjs({}),
+      //      babel({
+      //        runtimeHelpers: true,
+      //        exclude: '../../node_modules/**',
+      //        configFile: '../../babel.config.js',
+      //      }),
     ],
   }
 }
