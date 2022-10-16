f80e90d67b49d69e5281930a7d5b9e1600209822
Hannes Elvemyr <hannes@accodeing.com>
2022-10-16 12:32:26 +0200

    Adds icon and styles to accordion

--- components/accordion/src/index.js
+++ components/accordion/src/index.js
@@ -2,23 +2,46 @@ import { component, html } from 'haunted'
 
 const ELEMENT = 'oma-accordion'
 
-const Accordion = () => {
+const Accordion = ({ appendicon = false }) => {
+  const summarySlot = html`
+    <slot name="summary"></slot>
+  `
+  const iconSlot = html`
+    <slot name="icon"></slot>
+  `
+
+  const summaryContent = appendicon
+    ? html`
+        ${summarySlot}${iconSlot}
+      `
+    : html`
+        ${iconSlot}${summarySlot}
+      `
+
   return html`
     <style>
+      details {
+        background: var(--oma-accordion__background);
+        border: var(--oma-accordion__details-border);
+      }
+
       summary {
         cursor: pointer;
+        background: var(--oma-accordion__summary-background);
       }
+
       summary::marker {
         content: var(--oma-accordion__marker-content, none);
       }
     </style>
+
     <details>
-      <summary>
-        <slot name="summary"></slot>
-      </summary>
+      <summary>${summaryContent}</summary>
       <slot name="details"></slot>
     </details>
   `
 }
+
+Accordion.observedAttributes = ['appendicon']
 
 customElements.define(ELEMENT, component(Accordion))
