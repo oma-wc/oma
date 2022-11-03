10b142aae996ad6143047e82b53012d5a53c79a1
Hannes Elvemyr <hannes@accodeing.com>
2022-11-04 00:26:54 +0100

    Adds clerk component

---
+++ components/clerk/src/index.js
@@ -0,0 +1,123 @@
+import { component, html, useState } from 'haunted'
+
+import ClientLibrary from './client-library.js'
+
+/**
+ * NOTE: A lot of this code also exists in @formclerk/react
+ */
+const Clerk = ({ clerkId }) => {
+  const INITIALIZED = 0
+  const PROCESSING = 1
+  const SUCCESS = 2
+  const FAILURE = 3
+
+  const url = `https://europe-west1-accodeing-saas.cloudfunctions.net/clerk_enqueue/${clerkId}`
+  const form = document.querySelector('[slot="form"]')
+  const clientLibraryOnSubmit = new ClientLibrary(form, { autobind: false})
+  const [status, setStatus] = useState(INITIALIZED)
+
+  const dataFromFormElements = (elements) => {
+    let data = {}
+
+    Array.from(elements)
+      .filter((element) => {
+        if (element.nodeName === 'INPUT') {
+          if (element.type === 'radio' || element.type === 'checkbox') {
+            return element.checked // Only add checked ones
+          } else if (element.type === 'hidden') {
+            return true
+          } else if (element.type === 'submit') {
+            return false
+          } else {
+            // All other input types, like text and number
+            return element.value !== '' // Only add if field has value
+          }
+        } else if (element.nodeName === 'TEXTAREA') {
+          if (element.id === 'comment') {
+            return true
+          } else {
+            return element.value !== '' // Only add if field has value
+          }
+        } else if (element.nodeName === 'SELECT') {
+          return element.value !== '' // Only add if a value is selected
+        }
+      })
+      .map((element) => {
+        if (element.type === 'checkbox') {
+          if (data[element.name] === undefined) {
+            data[element.name] = []
+          }
+          data[element.name].push(element.value)
+        } else {
+          data[element.name] = element.value
+        }
+      })
+
+    return data
+  }
+
+  const postData = (url, data) =>
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+
+  const submitForm = (event) => {
+    event.preventDefault()
+
+    setStatus(PROCESSING)
+
+    clientLibraryOnSubmit()
+
+    console.log('Clerk: Posting data...')
+
+    const data = dataFromFormElements(event.target.elements)
+
+    postData(url, data)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Clerk success:', data)
+        setStatus(SUCCESS)
+      })
+      .catch((error) => {
+        console.error('Clerk error:', error)
+        setStatus(FAILURE)
+      })
+  }
+  form.onsubmit = submitForm
+
+  const formSlot = html`<slot name="form"></slot>`
+
+  if (status === PROCESSING) {
+    return html`
+      ${formSlot} 
+      <slot name="processing"></slot>
+    `
+  }
+
+  if (status === FAILURE) {
+    return html`
+      ${formSlot} 
+      <slot name="failure"></slot>
+    `
+  }
+
+  if (status === SUCCESS) {
+    return html`
+      ${formSlot} 
+      <slot name="sucess"></slot>
+    `
+  }
+
+  return html`
+    ${formSlot} 
+  `
+}
+
+Clerk.observedAttributes = ['clerk-id']
+
+customElements.define('oma-clerk', component(Clerk))
