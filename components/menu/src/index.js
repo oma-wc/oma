4035bcd8f7fb290e93a27030a7d47c9039e9caff
Hannes Elvemyr <hannes@accodeing.com>
2022-11-13 09:26:35 +0100

    Removes alternative-items unused props from menu

--- components/menu/src/index.js
+++ components/menu/src/index.js
@@ -1,18 +1,14 @@
 import { component, html, useState, useEffect } from 'haunted'
 
 const Menu = (element) => {
-  const { alignMenus, alignButtons } = element
   const [open, setOpen] = useState(false)
   const button = element.querySelector('[slot=button]')
   const items = element.querySelector('[slot=items]')
-  const alternativeItems = element.querySelector('[slot=alternative-items]')
 
   button.classList.add('oma-menu__button')
   items.classList.add('oma-menu__items')
-  alternativeItems.classList.add('oma-menu__alternative-items')
 
   items.classList.toggle('oma-menu__items--open', open)
-  alternativeItems.classList.toggle('oma-menu__alternative-items--open', open)
 
   useEffect(() => {
     const onClick = () => {
@@ -30,11 +26,8 @@ const Menu = (element) => {
     <nav>
       <slot name="button"></slot>
       <slot name="items"></slot>
-      <slot name="alternative-items"></slot>
     </nav>
   `
 }
-
-Menu.observedAttributes = ['align-menus', 'align-buttons']
 
 customElements.define('oma-menu', component(Menu))
